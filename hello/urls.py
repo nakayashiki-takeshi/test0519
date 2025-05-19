9821b80c262e0b208a1c447adb0c6e57b6e42d5e
nakayashiki <tn.ds26591026@signifi.co.jp>
2025-05-19 23:48:06 +0900

    v1

---
+++ hello/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import hello_world, GreetingView, HelloView, GoodbyeView
+
+urlpatterns = [
+	path('', hello_world),				    	# 関数「hello_world」を実行
+    path('greet/', GreetingView.as_view()),		#「 GreetingView」 を関数化して実行
+	path('hello/', HelloView.as_view()),		#「 HelloView」 を関数化して実行
+	path('goodbye/', GoodbyeView.as_view()),	#「 GoodbyeView」 を関数化して実行
+]
---
+++ hello/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView	#HTML表示用の汎用ビュー
+
+
+
+def hello_world(request):				    	# 関数「hello_world」
+	return HttpResponse("Hello, World!")    	# HttpResponseオブジェクトの生成
+
+
+
+class GreetingView(TemplateView):		    	# クラス「GreetingView」
+	template_name = 'greeting.html'		        # テンプレートファイルの指定（※ TemplateViewの機能）
+	message = 'ご挨拶申し上げます'
+
+	def get_context_data(self, **kwargs):   	# htmlテンプレートへ動的にデータを渡す場合に記述
+		context = super().get_context_data(**kwargs)  # 親クラスから基本的なコンテキストを取得
+		context['message'] = self.message   	#「message」をコンテキストに追加　（※html側では {{ message }}で呼び出し）
+		return context
+
+class HelloView(GreetingView):		           	# クラス「GreetingView」を継承
+	message = 'こんにちは！'		        	 # コンテキスト内「message」の値を変更
+
+class GoodbyeView(GreetingView):		    	# クラス「GreetingView」を継承
+	message = 'さようなら！'                     # コンテキスト内「message」の値を変更
+
+
+
+
