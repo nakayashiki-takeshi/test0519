e1810693c2ccbcfd515b4f389d6ab0b81df213c4
nakayashiki <tn.ds26591026@signifi.co.jp>
2025-05-26 23:07:12 +0900

    v12

--- hello/urls.py
+++ hello/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import hello_world, GreetingView, HelloView, GoodbyeView
 
 urlpatterns = [
-	path('', hello_world),				    	# 関数「hello_world」を実行
-    path('greet/', GreetingView.as_view()),		#「 GreetingView」を関数化して実行
-	path('hello/', HelloView.as_view()),		#「 HelloView」 　を関数化して実行
-	path('goodbye/', GoodbyeView.as_view()),	#「 GoodbyeView」 を関数化して実行
+	path('', hello_world),										 # 関数「hello_world」を実行
+    path('greet/', GreetingView.as_view(), name='greet-home'),	 #「 GreetingView」を関数化して実行
+	path('hello/', HelloView.as_view(), name='hello-home'),		 #「 HelloView」 　を関数化して実行
+	path('goodbye/', GoodbyeView.as_view(), name='goodby-home'), #「 GoodbyeView」 を関数化して実行
 ]
 
 
